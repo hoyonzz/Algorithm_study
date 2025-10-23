06b8a231288f86258477c4921c65a825379fc0f0
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2025-10-23 21:40:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.67 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,10 @@
-from collections import Counter
+from collections import defaultdict
 def solution(participant, completion):
-    
-    return list(Counter(participant) - Counter(completion))[0]
+    runners = defaultdict(int)
+    for runner in participant:
+        runners[runner] += 1
+    for runner in completion:
+        runners[runner] -= 1
+    for runner, i in runners.items():
+        if runners[runner] == 1:
+            return runner
