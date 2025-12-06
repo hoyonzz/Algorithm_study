2c2d4c473c8bcbca7b319c75c3d4114f72c26f04
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2025-12-06 20:15:31 +0900

    [Bronze V] Title: 시험 성적, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,8 @@
+import sys
+input = sys.stdin.readline
 score = int(input())
-if score >= 90:
-    print('A')
-elif score >= 80:
-    print('B')
-elif score >= 70:
-    print('C')
-elif score >= 60:
-    print('D')
-else:
-    print('F')
+if score >= 90: print('A')
+elif score >= 80: print('B')
+elif score >= 70: print('C')
+elif score >= 60: print('D')
+else: print('F')
