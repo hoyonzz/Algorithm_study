cf644c3d0fc859f385af324a7c1e93fc5fe00611
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2025-12-06 22:24:09 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,10 +1,4 @@
-import sys
-input = sys.stdin.readline
-mods = [0] * 42
+mods=set()
 for _ in range(10):
-    mods[int(input())%42]+=1
-result = 0
-for i in range(len(mods)):
-    if mods[i] >= 1:
-        result += 1
-print(result)
+    mods.add(int(input())%42)
+print(len(mods))
