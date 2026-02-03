c5d872b731db2ed20c5a1af5ec3719fb461d2932
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2026-02-03 16:46:23 +0900

    [Silver I] Title: 미로 탐색, Time: 56 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,28 @@
 import sys
 from collections import deque
 
-
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-maps = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+board = []
 for _ in range(n):
     row = list(map(int, input().strip()))
-    maps.append(row)
+    board.append(row)
     
-def BFS(x, y):
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
     queue = deque([(x, y)])
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if not(0<=nx<n and 0<=ny<m):
                 continue
-            if maps[nx][ny] == 1:
+            if board[nx][ny] == 1:
+                board[nx][ny] = board[x][y] + 1
                 queue.append((nx, ny))
-                maps[nx][ny] = maps[x][y] + 1
-
-BFS(0, 0)
-print(maps[n-1][m-1])
+bfs(0, 0)
+print(board[n-1][m-1])
