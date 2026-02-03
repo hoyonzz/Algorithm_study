1f9ea90c2411c09e49dcf1060a0e73c00f3b1404
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2026-02-03 18:05:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,48 +1,42 @@
-from collections import deque
 import sys
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 n = int(input())
-maps = []
+board = []
+
 for _ in range(n):
     row = list(map(int, input().strip()))
-    maps.append(row)
-
+    board.append(row)
+    
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-result = []
+visited = [[False] * n for _ in range(n)]
 
-visited = [[0] * n for _ in range(n)]
-
-def BFS(x, y):
-    count = 1
-    queue = deque([(x, y)])
+def dfs(x, y):
+    global count
     visited[x][y] = True
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if not visited[nx][ny]:
-                visited[nx][ny] = 1
+    count += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if not visited[nx][ny] and board[nx][ny] == 1:
+                dfs(nx, ny)
 
-                if maps[nx][ny] == 1:
-                    count += 1
-                    queue.append((nx, ny))
-    return count
+result = []
 
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] and maps[i][j] == 1:
-            result.append(BFS(i, j))
+        if not visited[i][j] and board[i][j] == 1:
+            count = 0
+            dfs(i, j)
+            result.append(count)
+            
+result.sort()
 
 print(len(result))
-result.sort()
 for i in result:
     print(i)
-                    
-                
