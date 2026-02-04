84e38109094e01fdefb26793a50b7c7da82ea74b
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2026-02-04 17:28:47 +0900

    [Gold V] Title: 토마토, Time: 1864 ms, Memory: 99596 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,37 +3,38 @@ from collections import deque
 
 input = sys.stdin.readline
 
-queue = deque()
-graph = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
 m, n = map(int, input().split())
+box = []
+
+queue = deque()
 
 for i in range(n):
     row = list(map(int, input().split()))
-    graph.append(row)
+    box.append(row)
     for j in range(m):
         if row[j] == 1:
             queue.append((i, j))
-    
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 while queue:
     x, y = queue.popleft()
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx < 0 or ny < 0 or nx >= n or ny >= m:
-            continue
-        if graph[nx][ny] == 0:
-            queue.append((nx, ny))
-            graph[nx][ny] = graph[x][y] + 1
+        if 0 <= nx < n and 0 <= ny < m:
+            if box[nx][ny] == 0:
+                queue.append((nx, ny))
+                box[nx][ny] = box[x][y] + 1
 
 result = 0
+
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 0:
+        if box[i][j] == 0:
             print(-1)
             exit()
-        else:
-            result = max(result, graph[i][j])
-print(result -1)
+        result = max(result, box[i][j])
+
+print(result - 1)
