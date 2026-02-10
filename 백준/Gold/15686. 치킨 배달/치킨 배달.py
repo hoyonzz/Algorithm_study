276c79b48e5b1be5a5f4496cd227ea4758b48078
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2026-02-10 17:28:50 +0900

    [Gold V] Title: 치킨 배달, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,13 +1,12 @@
 import sys
 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 n, m = map(int, input().split())
-
 chicken_houses = []
 houses = []
 selected = []
+result = float('inf')
 
 for i in range(n):
     row = list(map(int, input().split()))
@@ -17,36 +16,26 @@ for i in range(n):
         elif row[j] == 1:
             houses.append((i+1, j+1))
 
-# 집마다 치킨거리를 구하는 함수
-def get_chicken_dist():
-    total_dist = 0
-    # 집들은 고정
-    for i in range(len(houses)):
-        hr, hc = houses[i]
-        min_chicken_dist = float('inf')
-        for cr, cc in selected:
-            dist = abs(hr-cr) + abs(hc-cc)
-            min_chicken_dist = min(min_chicken_dist, dist)
-        total_dist += min_chicken_dist
-    return total_dist
-
-result = float('inf')
-# 백트레킹 함수
-def backtracking(idx, count):
+def backtracking(count, idx):
     global result
-    # 종료조건
     if count == m:
         result = min(result, get_chicken_dist())
         return
-    
-    # 치킨집 순회(백트레킹)
+
     for i in range(idx, len(chicken_houses)):
         selected.append(chicken_houses[i])
-
-        backtracking(i+1, count+1)
-
+        backtracking(count+1, i+1)
         selected.pop()
 
-backtracking(0, 0)
+def get_chicken_dist():
+    total_dist = 0
+    for hr, hc in houses:
+        min_dist = float('inf')
+        for cr, cc in selected:
+            dist = abs(hr-cr) + abs(hc-cc)
+            min_dist = min(min_dist, dist)
+        total_dist += min_dist
+    return total_dist
 
+backtracking(0, 0)
 print(result)
