8846959af8871aab620ca79060c0234a698f73a6
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-03-16 22:44:57 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,40 +1,49 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-x, y, d = map(int, input().split())
-maps = []
 
+n, m = map(int, input().split())
+r, c, d = map(int, input().split())
+board = []
 for _ in range(n):
     row = list(map(int, input().split()))
-    maps.append(row)
-    
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+    board.append(row)
 
 answer = 0
 
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+
 while True:
-    if maps[x][y] == 0:
-        maps[x][y] = 2
+    # 자리 청소
+    if board[r][c] == 0:
+        board[r][c] = 2
         answer += 1
-    is_moved = False
+    
+    # 주변 탐색
+    possible_cleaning = False
+
     for i in range(4):
-        d = (d-1) % 4
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < m:
-            if maps[nx][ny] == 0:
-                x, y = nx, ny
-                is_moved = True
-                break
-    if is_moved:
-        continue
-    else:
-        nx = x - dx[d]
-        ny = y - dy[d]
-        if maps[nx][ny] == 1:
+        nr = r + dr[i]
+        nc = c + dc[i]
+        if board[nr][nc] == 0:
+            possible_cleaning = True
             break
-        x, y = nx, ny
+    
+    if not possible_cleaning:
+        br = r - dr[d]
+        bc = c - dc[d]
+        if board[br][bc] != 1:
+            r, c = br, bc
+        else:
+            break
+
+    else: # 주변에 청소할 공간이 있다면,
+        # 반시계 방향 회전
+        d = (d-1) % 4
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if board[nr][nc] == 0:
+            r, c = nr, nc
 
 print(answer)
