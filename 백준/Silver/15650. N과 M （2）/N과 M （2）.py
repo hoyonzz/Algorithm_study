9037fa28da34d3fe39f591c0043024c95d982224
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-03-16 20:32:40 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -4,13 +4,13 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 result = []
 
-def backtracking(depth, start):
-    if depth == m:
+def backtracking(start):
+    if len(result) == m:
         print(*result)
         return
     for i in range(start, n+1):
         result.append(i)
-        backtracking(depth+1, i+1)
+        backtracking(i+1)
         result.pop()
 
-backtracking(0, 1)
+backtracking(1)
