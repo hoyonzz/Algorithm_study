f21c87a43c4c81047996c12500547a680906cfca
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-03-31 17:18:41 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 604 ms, Memory: 120700 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,18 +1,13 @@
 import sys
 input = sys.stdin.readline
 
+from collections import Counter
+
 n = int(input())
-nums = list(map(int, input().split()))
-dict_nums = {}
-for i in range(n):
-    if nums[i] in dict_nums:
-        dict_nums[nums[i]] += 1
-    else:
-        dict_nums[nums[i]] = 1
-
+keys = list(map(int, input().split()))
 m = int(input())
-target = list(map(int, input().split()))
+values = list(map(int, input().split()))
 
-for i in range(m):
-    print(dict_nums.get(target[i], 0), end = ' ')
+keys_counter = Counter(keys)
 
+print(*((keys_counter.get(x, 0) for x in values)))
