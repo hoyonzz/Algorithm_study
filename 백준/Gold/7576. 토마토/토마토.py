aff10ae6749a892747de1fe3d569b82c9b7a5018
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-03-31 14:46:29 +0900

    [Gold V] Title: 토마토, Time: 2492 ms, Memory: 99596 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,16 +1,16 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
+from collections import deque
+
 m, n = map(int, input().split())
-box = []
+board = []
 
 queue = deque()
 
 for i in range(n):
     row = list(map(int, input().split()))
-    box.append(row)
+    board.append(row)
     for j in range(m):
         if row[j] == 1:
             queue.append((i, j))
@@ -24,17 +24,17 @@ while queue:
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < n and 0 <= ny < m:
-            if box[nx][ny] == 0:
+            if board[nx][ny] == 0:
+                board[nx][ny] = board[x][y] + 1
                 queue.append((nx, ny))
-                box[nx][ny] = box[x][y] + 1
 
-result = 0
+answer = 0 
 
 for i in range(n):
     for j in range(m):
-        if box[i][j] == 0:
+        if board[i][j] == 0:
             print(-1)
             exit()
-        result = max(result, box[i][j])
+        answer = max(answer, board[i][j])
 
-print(result - 1)
+print(answer-1)
