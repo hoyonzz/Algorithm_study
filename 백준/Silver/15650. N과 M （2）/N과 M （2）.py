c64144c1852aa30b6a9c0c862bcfbe0194afeb34
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-03-31 15:39:55 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -8,9 +8,10 @@ def backtracking(start):
     if len(result) == m:
         print(*result)
         return
+    
     for i in range(start, n+1):
         result.append(i)
-        backtracking(i+1)
+        backtracking(i + 1)
         result.pop()
 
 backtracking(1)
