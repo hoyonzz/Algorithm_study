2a323ca33741874bf42797f31f37170ba90a6dd3
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-04-08 17:27:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.36 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,27 +1,25 @@
 import re
 
 def solution(new_id):
-    # 1. 대문자를 소문자로 치환
+    answer = ''
+    # 1단계_대문자를 소문자로 치환
     new_id = new_id.lower()
+    # 2단계_알파벳 소문자, 숫자, -, _, .를 제외한 문제 제거
+    for char in new_id:
+        if char.isalnum() or char in '-_.':
+            answer += char
+    # 3단계_마침표 2번 이상 -> 하나로
+    answer = re.sub(r"\.{2,}", ".", answer)
+    # 4단계_마침표가 처음이나 끝에 있다면 제거
+    answer = answer.strip('.')
+    # 5단계_빈 문자열이라면, a를 대입
+    if answer == '': 
+        answer += 'a'
+    # 6단계_길이 16자 이상이면, 제거
+    answer = answer[:15]
+    answer = answer.rstrip('.')
+    # 7단계_ 길이가 2자 이하라면, 반복해서 끝에 붙임
+    while len(answer) <= 2:
+        answer += answer[-1]
     
-    # 2. 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 모든 문자 제거
-    new_id = re.sub(r'[^a-z0-9\-_.]', '', new_id)
-    
-    # 3. 마침표 2번이상 연속된 부분 하나의 마침표로 치환
-    new_id = re.sub(r'\.{2,}', '.', new_id)
-    
-    # 4. 처음이나 끝에 . 있다면 제거
-    new_id = re.sub(r'^\.|\.$', '', new_id)
-    
-    # 5. 빈문자열이면 a대입
-    if not new_id:
-        new_id = 'a'
-    
-    # 6. 16자 이상이라면 15개만 남기고 마지막이 . 라면제거
-    new_id = new_id[:15].rstrip('.')
-    
-    # 7. 2자 이하라면 마지막 문자가 3글자가 될때까지 반복
-    while len(new_id) < 3:
-        new_id += new_id[-1]
-    
-    return new_id
+    return answer
