68e4da0d1e929c1574fad28d28ec65511aeb9ee6
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-04-15 01:43:22 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.35 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,25 +1,24 @@
-import re
-
 def solution(new_id):
+    import re
     answer = ''
-    # 1단계_대문자를 소문자로 치환
+    # 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
     new_id = new_id.lower()
-    # 2단계_알파벳 소문자, 숫자, -, _, .를 제외한 문제 제거
+    # 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
     for char in new_id:
-        if char.isalnum() or char in '-_.':
+        if char.isalnum() or char in ('-', '_', '.'):
             answer += char
-    # 3단계_마침표 2번 이상 -> 하나로
-    answer = re.sub(r"\.{2,}", ".", answer)
-    # 4단계_마침표가 처음이나 끝에 있다면 제거
+    # 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    answer = re.sub('\.{2,}', '.', answer)
+    # 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
     answer = answer.strip('.')
-    # 5단계_빈 문자열이라면, a를 대입
-    if answer == '': 
-        answer += 'a'
-    # 6단계_길이 16자 이상이면, 제거
+    # 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    if len(answer) == 0:
+        answer = 'a'
+    # 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    # 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
     answer = answer[:15]
     answer = answer.rstrip('.')
-    # 7단계_ 길이가 2자 이하라면, 반복해서 끝에 붙임
-    while len(answer) <= 2:
+    # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+    while len(answer) < 3:
         answer += answer[-1]
-    
     return answer
