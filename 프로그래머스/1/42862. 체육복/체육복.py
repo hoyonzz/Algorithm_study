57f65be6d613dfdaae10a0f5e4645c54e222ed14
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-04-21 17:29:12 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,14 +1,9 @@
 def solution(n, lost, reserve):
-    set_lost = set(lost) - set(reserve)
-    set_reserve = set(reserve) - set(lost)
-    answer = n - len(set_lost)
-    
-    for i in range(1, n+1):
-        if i in set_reserve:
-            if i-1 in set_lost:
-                set_lost.remove(i-1)
-                answer += 1
-            elif i+1 in set_lost:
-                set_lost.remove(i+1)
-                answer += 1
-    return answer
+    reserve_set = set(reserve) - set(lost)
+    lost_set = set(lost) - set(reserve)
+    for i in reserve_set:
+        if i-1 in lost_set:
+            lost_set.remove(i-1)
+        elif i+1 in lost_set:
+            lost_set.remove(i+1)
+    return n-len(lost_set)
