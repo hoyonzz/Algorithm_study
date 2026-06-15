a3d0a1d3208a733400094eeb2286233f522101a2
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-06-15 23:26:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.88 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,21 +1,22 @@
-from collections import deque
 def solution(maps):
-    n, m = len(maps), len(maps[0])
+    n = len(maps)
+    m = len(maps[0])
+    from collections import deque
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    queue = deque()
-    queue.append((0, 0))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx, ny))
+    def dfs(x, y):
+        queue = deque()
+        queue.append((x,y))
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < m:
+                    if maps[nx][ny] == 1:
+                        queue.append((nx, ny))
+                        maps[nx][ny] = maps[x][y] + 1
+    dfs(0, 0)
     if maps[n-1][m-1] == 1:
         return -1
-    else:
-        return maps[n-1][m-1]
+    return maps[n-1][m-1]
