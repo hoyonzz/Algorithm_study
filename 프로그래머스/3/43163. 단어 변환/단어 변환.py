829a44e54c1d86e33645f2585f15abc42164bb57
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-06-16 15:38:03 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,21 +1,29 @@
-from collections import deque
 def solution(begin, target, words):
+    def check_words(a, b):
+        check = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                check += 1
+        if check == 1:
+            return True
+        return False
+    
+    visited = [False] * len(words)
+    answer = 0
+    
+    from collections import deque
+    def bfs(start, count):
+        queue = deque()
+        queue.append((start, 0))
+        while queue:
+            word, count = queue.popleft()
+            if word == target:
+                return count
+            for i in range(len(words)):
+                if not visited[i] and check_words(word, words[i]):
+                    queue.append((words[i], count + 1))
+                    visited[i] = True
+    answer = bfs(begin, 0)
     if target not in words:
         return 0
-    queue = deque()
-    visited = [False] * len(words)
-    queue.append((begin, 0))
-    while queue:
-        word, step = queue.popleft()
-        if word == target:
-            return step
-        for i in range(len(words)):
-            if not visited[i]:
-                count = 0
-                for j in range(len(begin)):
-                    if word[j] != words[i][j]:
-                        count += 1
-                if count == 1:
-                    visited[i] = True
-                    queue.append((words[i], step +1 ))
-    return 0
+    return answer
