eb5e7690e893d78f4378f3c3b63f8bd7afc6a57a
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-06-16 17:35:35 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,8 @@
 def solution(array, commands):
     answer = []
-    for i in range(len(commands)):
-        arr = array[commands[i][0]-1:commands[i][1]]
+    for com in commands:
+        i, j, k = com[0], com[1], com[2]
+        arr = array[i-1:j]
         arr.sort()
-        answer.append(arr[commands[i][2]-1])
+        answer.append(arr[k-1])
     return answer
