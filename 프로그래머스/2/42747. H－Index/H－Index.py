0c57ef26ef2fcfe3ab8441ade2e77bfc95692e3d
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-06-17 15:31:32 +0900

    [level 2] Title: H-Index, Time: 0.13 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,10 @@
 def solution(citations):
-    answer= 0
-    citations.sort(reverse=1)
-    for i in range(len(citations)):
+    citations.sort(reverse=True)
+    n = len(citations)
+    h = 0
+    for i in range(n):
         if citations[i] >= i+1:
-            answer += 1
+            h += 1
         else:
             break
-
-    return answer
+    return h
