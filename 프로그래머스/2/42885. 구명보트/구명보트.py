09b03e26cc12a67c13f591d69c878a48367c83b0
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-06-26 20:36:55 +0900

    [level 2] Title: 구명보트, Time: 9.66 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,13 @@
 def solution(people, limit):
-    # 정렬
+    s, e = 0, len(people)-1
     people.sort()
     answer = 0
-    # 투포인터 설정
-    left, right = 0, len(people) - 1
-    # 투포인터 연산
-    while left <= right:
-        if left == right:
-            answer += 1
-            break
-        weight = people[left] + people[right]
-        if limit >= weight:
-            answer += 1
-            left, right = left + 1, right - 1
+    while s <= e:
+        if people[s] + people[e] > limit:
+            e -= 1
         else:
-            answer += 1
-            right = right - 1
+            s += 1
+            e -= 1
+        answer += 1
+        
     return answer
