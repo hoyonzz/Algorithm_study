47a9b3635c40835f93c6ae2bcbab666e92721991
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-06-26 17:06:31 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,18 +1,15 @@
 def solution(name):
     n = len(name)
-    answer = 0
-    
-    # 조이스틱 상하 이동
+    updown_move = 0
     for c in name:
-        if c != 'A':
-            answer += min(ord(c)-ord('A'), ord('Z') - ord(c) + 1)
-        
-    # 조이스틱 좌우 이동
+        updown_move += min(ord(c)-ord('A'), ord('Z')-ord(c)+1)
+
     min_move = n-1
     for i in range(n):
         next_idx = i+1
-        while next_idx < n and name[next_idx] =='A':
+        while next_idx < n and name[next_idx] == 'A':
             next_idx += 1
-        min_move = min(min_move, (i*2)+(n-next_idx), 2*(n-next_idx) + i)
-    print(answer, min_move)
-    return answer+min_move
+        min_move = min(min_move, (i*2) + (n-next_idx), 2*(n-next_idx) + i)
+        
+        
+    return updown_move+min_move
