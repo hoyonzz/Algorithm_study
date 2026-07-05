d0ff6d850d4e3f069d3550a7a10737690f84d7e7
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-07-05 17:59:59 +0900

    [level 2] Title: 가장 큰 수, Time: 30.33 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,7 @@
 def solution(numbers):
     arr = [str(x) for x in numbers]
-    arr.sort(reverse=True, key=lambda x: x * 3)
+    arr.sort(reverse=True, key=lambda x: x*3)
     if arr[0] == '0':
         return '0'
-    return ''.join(arr)
+    answer = ''.join(arr)
+    return answer
