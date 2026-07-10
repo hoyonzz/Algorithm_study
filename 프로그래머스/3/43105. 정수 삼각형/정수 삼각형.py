49afe85fc180dab77c0415973925b526f17dfcb7
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-07-10 15:38:34 +0900

    [level 3] Title: 정수 삼각형, Time: 25.47 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,7 +1,10 @@
 def solution(triangle):
     for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
-            left_up = triangle[i-1][j-1] if j > 0 else 0
-            right_up = triangle[i-1][j] if j < len(triangle[i])-1 else 0
-            triangle[i][j] += max(left_up, right_up)
+            up = triangle[i-1][j] if j < len(triangle[i]) - 1 else 0
+            up_left = triangle[i-1][j-1] if j > 0 else 0
+
+            triangle[i][j] += max(up_left, up)
+            
+            
     return max(triangle[len(triangle)-1])
