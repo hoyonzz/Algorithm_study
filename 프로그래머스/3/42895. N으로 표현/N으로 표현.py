2fd4ada6f2ed88945c4aa9a12a5b649063196e51
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-07-10 15:21:47 +0900

    [level 3] Title: N으로 표현, Time: 6.81 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,28 +1,18 @@
 def solution(N, number):
-    if N == number:
-        return 1
-    
     dp = [set() for _ in range(9)]
     
     for i in range(1, 9):
-        # 숫자 이어붙이기
-        dp[i].add(int(str(N) * i))
+        dp[i].add(int(str(N)*i))
         
-        # 이전 숫자 4칙연산
         for j in range(1, i):
             for num1 in dp[j]:
                 for num2 in dp[i-j]:
-                    # 더하기
                     dp[i].add(num1+num2)
-                    # 빼기
                     dp[i].add(num1-num2)
-                    # 나누기 (zero오류 방지)
-                    if num2 == 0:
-                        continue
-                    else:
+                    dp[i].add(num1*num2)
+                    if num2 != 0:
                         dp[i].add(num1//num2)
-                    # 곱하기
-                    dp[i].add(num1 * num2)
-            if number in dp[i]:
-                return i
+        if number in dp[i]:
+            return i
+        
     return -1
