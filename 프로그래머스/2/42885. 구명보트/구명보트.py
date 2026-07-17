5b679e3be1ed8ef2c6c6f57001f12ec406f49c91
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-07-17 20:09:55 +0900

    [level 2] Title: 구명보트, Time: 7.43 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,10 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    start, end = 0, len(people) - 1
-    while start <= end:
-        if people[start] + people[end] <= limit:
-            start += 1
-            end -= 1
-            answer += 1
-        else:
-            end -= 1
-            answer += 1
+    s, e = 0, len(people)-1
+    while s <= e:
+        if people[s] + people[e] <= limit:
+            s += 1
+        e -= 1
+        answer += 1
     return answer
