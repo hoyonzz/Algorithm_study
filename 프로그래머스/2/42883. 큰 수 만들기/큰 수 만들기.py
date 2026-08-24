de9bb282da612257294f1ade1bbb40ca6df42854
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-08-24 21:21:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 39.05 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,9 +1,9 @@
 def solution(number, k):
-    stack=[]
+    stack = []
     for num in number:
         while stack and k>0 and num>stack[-1]:
             stack.pop()
-            k-=1
+            k-= 1
         stack.append(num)
     if k > 0:
         stack = stack[:-k]
