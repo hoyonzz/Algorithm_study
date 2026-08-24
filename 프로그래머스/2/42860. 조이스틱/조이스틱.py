df22eb2feb09170a98e2bf61c60868b341e50388
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-08-24 20:33:47 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,18 +1,23 @@
 def solution(name):
+    n = len(name)
     updown_move = 0
     for c in name:
-        updown_move += min(ord(c)-ord('A'), ord('Z')-ord(c)+1)
-        
-    n = len(name)
+        if c == "A":
+            continue
+        updown_move += min(
+            ord(c) - ord("A"),
+            ord('Z') - ord(c) + 1
+        )
+    
     min_move = n-1
-    for i in range(0, n-1):
-        next_i = i+1
+    for i in range(n-1):
+        next_i = i + 1
         while next_i < n and name[next_i] == 'A':
             next_i += 1
         min_move = min(
             min_move,
-            (i*2)+n-next_i,
-            (n-next_i)*2 + i
+            (i*2) + n- next_i,
+            (n-next_i) * 2 + i
         )
     
     return updown_move + min_move
