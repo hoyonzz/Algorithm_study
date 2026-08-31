1bb97332db0f432681f05a049c24fff968254471
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-08-31 17:02:12 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,13 +1,15 @@
 def solution(n, computers):
-    answer = 0
     visited = [False] * n
+    
     def dfs(v):
         visited[v] = True
-        for i in range(len(computers[v])):
-            if not visited[i] and computers[v][i] == 1:
+        for i in range(n):
+            if computers[v][i] == 1 and not visited[i]:
                 dfs(i)
+    answer = 0
     for i in range(n):
         if not visited[i]:
             dfs(i)
             answer += 1
     return answer
+            
