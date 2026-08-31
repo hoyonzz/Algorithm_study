a292ad708456da7224b6ef9704b02ebc52174754
Hoyong <129498722+hoyonzz@users.noreply.github.com>
2026-08-31 17:45:35 +0900

    [level 3] Title: 단어 변환, Time: 0.25 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,27 +1,22 @@
+from collections import deque
 def solution(begin, target, words):
     if target not in words:
         return 0
     
-    def check_word(word1, word2):
-        count = 0
-        for i in range(len(word1)):
-            if word1[i] != word2[i]:
-                count += 1
-        if count == 1:
-            return True
-        return False
-    
-    from collections import deque
-    queue = deque([(begin, 0)])
     visited = set([begin])
-    while queue:
-        current_word, current_step = queue.popleft()
-        if current_word == target:
-            return current_step
-        for word in words:
-            if word not in visited:
-                if check_word(current_word, word):
-                    queue.append((word, current_step+1))
-                    visited.add(word)
     
-    return 0
+    def bfs(word, cnt):
+        queue = deque()
+        queue.append((word, cnt))
+        while queue:
+            w, step = queue.popleft()
+            if w == target:
+                return step
+            
+            for next_word in words:
+                if next_word not in visited and sum(c1 != c2 for c1, c2 in zip(w, next_word)) == 1:
+                    visited.add(next_word)
+                    queue.append((next_word, step+1))
+        return 0
+    
+    return bfs(begin, 0)
